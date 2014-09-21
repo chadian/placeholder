a2f3f21836521a6e79f7b5047f0250efcb86145f
Chad Carbert <chadcarbert@me.com>
2014-09-21 13:59:54 -0600

    Added visually better examples for sass and vanilla js. Example page has some styling and layout added for the page theme, too.

--- examples/js/vanilla-placeholder-examples.js
+++ examples/js/vanilla-placeholder-examples.js
@@ -1,24 +1,24 @@
 var element_width_px_height_px = document.getElementById('vanilla-width-px-height-px');
 var placeholder_width_px_height_px_element = new PlaceholderVanilla(element_width_px_height_px, {
-	width: '300px',
-	height: '125px'
+	width: '450px',
+	height: '235px'
 }).applyDimensions();
 
 var element_width_per_height_per = document.getElementById('vanilla-width-per-height-per');
 var placeholder_width_per_height_per = new PlaceholderVanilla(element_width_per_height_per, {
-	width: '50%',
-	height: '35%'
+	width: '30%',
+	height: '150%'
 }).applyDimensions();
 
 var element_width_px_height_per = document.getElementById('vanilla-width-px-height-per');
 var placeholder_width_px_height_per = new PlaceholderVanilla(element_width_px_height_per, {
-	width: '200px',
+	width: '600px',
 	height: '50%'
 }).applyDimensions();
 
 var element_width_per_height_px = document.getElementById('vanilla-width-per-height-px');
 var placeholder_width_per_height_px = new PlaceholderVanilla(element_width_per_height_px, {
-	width: '25%',
+	width: '80%',
 	height: '200px'
 }).applyDimensions();
 
@@ -29,12 +29,12 @@ var placeholder_element_ratio_only = new PlaceholderVanilla(element_ratio_only, 
 
 var element_ratio_with_width_px = document.getElementById('vanilla-ratio-with-width-px');
 var placeholder_ratio_with_width_px = new PlaceholderVanilla(element_ratio_with_width_px, {
-	width: '150px',
-	ratio: 0.65
+	width: '350px',
+	ratio: 1.25
 }).applyDimensions();
 
 var element_ratio_with_width_per = document.getElementById('vanilla-ratio-with-width-per');
 var placeholder_ratio_with_width_per = new PlaceholderVanilla(element_ratio_with_width_per, {
-	width: '25%',
-	ratio: 0.35
+	width: '45%',
+	ratio: 1
 }).applyDimensions();
